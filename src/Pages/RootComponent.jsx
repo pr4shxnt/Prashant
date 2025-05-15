96d3e7ecead01f839b59f0ae976e8dbeee87ad06
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-05-15 07:33:23 +0545

    under maintenance

--- src/Pages/RootComponent.jsx
+++ src/Pages/RootComponent.jsx
@@ -1,21 +1,12 @@
 import InitialRender from './InitialRender';
 import { Outlet } from 'react-router-dom';
 import { useContent } from '../Utils/ContextProvider';
-import ProdBuild from './ProdBuild';
-import { useState } from 'react';
 
 const RootComponent = () => {
   const { showContent, setShowContent } = useContent();
-const [showCont, setShowCont] = useState(false);
-
-const production = import.meta.env.NODE_ENV;
-
-if (production === 'production') {
-      return <div><ProdBuild/></div>
-}
 
 
-else{
+
   return (
     <>
        <div className=" ">
@@ -24,13 +15,11 @@ else{
 
        {showContent && (
         <>
-
-          
-          <Outlet />
+                  <Outlet />
         </>
       )}
     </>
-  );}
+  );
 };
 
 export default RootComponent;
