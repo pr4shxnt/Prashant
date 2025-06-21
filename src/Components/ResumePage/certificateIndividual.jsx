d19532caa5f9df7a116ae5e5275866ac5514282b
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-06-21 22:16:04 +0545

    title push

---
+++ src/Components/ResumePage/certificateIndividual.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createPortal } from 'react-dom'
+
+const certificateIndividual = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default certificateIndividual
