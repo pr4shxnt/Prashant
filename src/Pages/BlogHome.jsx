0283f656b1f4e04001f0d0b821a828b5638dac1e
pr4shxnt <student@prashantadhikari7.com.np>
2025-12-03 09:10:58 +0545

    Frontend: gaand faad opening

--- src/Pages/BlogHome.jsx
+++ src/Pages/BlogHome.jsx
@@ -4,43 +4,43 @@ import LatestBlog from '../Components/BlogPage/LatestBlog'
 import FeaturedBlogs from '../Components/BlogPage/BlogCard'
 import RecentlyAdded from '../Components/BlogPage/RecentlyAdded'
 
-const BlogHome = () => {
-    if (import.meta.env.MODE === 'production') {
+// const BlogHome = () => {
+//     if (import.meta.env.MODE === 'production') {
  
   
 
-  return (
-    <>
-      <title>Maintenance | Prashant Adhikari</title>
-      <div className='fixed z-[9999999999999999] text-white flex flex-col items-center justify-center bg-black h-screen w-screen'>
-        <h1 className='text-3xl text-center font-bold uppercase tracking-wide'>The website is under development phase.</h1>
-        <p className='text-center'>Feel free to contact the developer of this website.</p>
-        <div className=" social-links flex items-center z-[200] px-3 py-1">
-          <a target='_blank' title='email' href="mailto:prashantadhikareeey.dev@gmail.com" className="">
-            <img src="./email.png" alt="" className="w-6 m-2" />
-          </a>
-          <a target='_blank' title='whatsapp' href="https://wa.me/+9779742433049?text=Hello%20Prashant!" className="">
-            <img src="./whatsapp.png" alt="" className="w-5 m-2" />
-          </a>
-          <a target='_blank' title='instagram' href="https://www.instagram.com/pr4xnt" className="">
-            <img src="./instagram.png" alt="" className="w-8 m-1.5" />
-          </a>
-          <a target='_blank' title='github' href="https://www.github.com/pr4shxnt" className="">
-            <img src="./github.png" alt="" className="w-6 m-2"/>
-          </a>
-        </div>
-      </div>
-    </>
-  )
-}
-  return (
-    <>
-    <BlogHomePage/>
-    <FeaturedBlogs/>
-    <LatestBlog/>
-    <RecentlyAdded/>
-    </>
-)
-}
+//   return (
+//     <>
+//       <title>Maintenance | Prashant Adhikari</title>
+//       <div className='fixed z-[9999999999999999] text-white flex flex-col items-center justify-center bg-black h-screen w-screen'>
+//         <h1 className='text-3xl text-center font-bold uppercase tracking-wide'>The website is under development phase.</h1>
+//         <p className='text-center'>Feel free to contact the developer of this website.</p>
+//         <div className=" social-links flex items-center z-[200] px-3 py-1">
+//           <a target='_blank' title='email' href="mailto:prashantadhikareeey.dev@gmail.com" className="">
+//             <img src="./email.png" alt="" className="w-6 m-2" />
+//           </a>
+//           <a target='_blank' title='whatsapp' href="https://wa.me/+9779742433049?text=Hello%20Prashant!" className="">
+//             <img src="./whatsapp.png" alt="" className="w-5 m-2" />
+//           </a>
+//           <a target='_blank' title='instagram' href="https://www.instagram.com/pr4xnt" className="">
+//             <img src="./instagram.png" alt="" className="w-8 m-1.5" />
+//           </a>
+//           <a target='_blank' title='github' href="https://www.github.com/pr4shxnt" className="">
+//             <img src="./github.png" alt="" className="w-6 m-2"/>
+//           </a>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+//   return (
+//     <>
+//     <BlogHomePage/>
+//     <FeaturedBlogs/>
+//     <LatestBlog/>
+//     <RecentlyAdded/>
+//     </>
+// )
+// }
 
 export default BlogHome
